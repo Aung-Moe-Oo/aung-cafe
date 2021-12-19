aa09d8f893ec443f5aaa5776b6c0d1dc4e7ac4e7
Aung-Moe-Oo <70925607+Aung-Moe-Oo@users.noreply.github.com>
2021-12-19 13:53:13 +0630

    Update Products.jsx

--- components/Products.jsx
+++ components/Products.jsx
@@ -4,7 +4,7 @@ const Products = (props) => {
   return (
     <div className="bg-yellow-300 bg-opacity-30 z-0 lg:py-8 md:py-4 overflow-hidden">
       <div className="container lg:mx-auto mx-0">
-        <h1 className="text-center font-bold text-3xl pb-8">
+        <h1 className="text-center font-bold text-3xl pt-8 md:pt-4  pb-8">
           Featured Products
         </h1>
         <div className="flex flex-row flex-wrap justify-around relative z-10">
